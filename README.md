ddeb158a964767d13e684718318db61f709c4097
jmartinez-uv <jmartinez@unitedvirtualities.com>
2020-07-03 11:57:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Barbarian
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+## prerequisites 
+
+Dependencies
+
+```
+npm install --save contentful
+npm install @contentful/rich-text-html-renderer
+npm install algoliasearch
+npm install remove-markdown
+```
+
+
+## Contentful + NuxtJs
+
+...
+
+## Contentful + Algolia
+
+...
---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+
+const config = require('./.contentful.json')
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: ['~/plugins/contentful.js'
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  env: {
+    ALGOLIA_APP_ID: config.ALGOLIA_APP_ID,
+    ALGOLIA_ADMIN_KEY: config.ALGOLIA_ADMIN_KEY,
+    ALGOLIA_INDEX: config.ALGOLIA_INDEX,
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: ['@nuxtjs/markdownit'
+  ],
+  markdownit: {
+    injected: true
+},
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+    transpile: ['vue-instantsearch', 'instantsearch.js/es'],
+  }
+}
